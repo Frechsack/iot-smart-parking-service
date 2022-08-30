cbbfd55f2308402686042e1a4429daf2099d8d91
frechsack <patrickwolter.1@gmx.de>
2022-08-30 17:18:22 +0200

    Default

--- src/orm/entity/account.ts
+++ src/orm/entity/account.ts
@@ -9,7 +9,7 @@ export class Account {
   @Column({type: "varchar", primary: true, length: 50 })
   public email: string;
 
-  @Column({ type: "tinyint", nullable: false })
+  @Column({ type: "tinyint", nullable: false, default: false })
   public isAdmin: boolean;
 
   @Column({type: "varchar" })
